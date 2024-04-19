858bab9f1e86eeed6dd8481a9a98f0674801fbec
hamazoon <hamazoon@gmail.com>
2024-04-19 10:51:05 +0100

    file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,4 @@
-
-vnifest to Create and Configure a File
+#vnifest to Create and Configure a File
 
 file{ '/tmp/school':
 ensure  => 'file',
@@ -7,5 +6,4 @@ mode    => '0744',
 owner   => 'www-data',
 group   => 'www-data',
 content => 'I love Puppet',
-
 }
